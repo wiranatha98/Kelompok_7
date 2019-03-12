1eacff70f85e1cfbbd9f6b4d8539bb61d7e6f7ef
wiranatha98 <wiranatha98@gmail.com>
2019-03-12 17:06:44 +0700

    'Fitur Info v1.0'

--- app/src/main/java/com/example/edulib/Info.java
+++ app/src/main/java/com/example/edulib/Info.java
@@ -1,8 +1,10 @@
 package com.example.edulib;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Info extends AppCompatActivity {
 
@@ -11,6 +13,12 @@ public class Info extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         ActionBar ab = getSupportActionBar();
-        ab.setTitle("Info");
+        ab.hide();
+
+    }
+
+    public void infoUN(View view) {
+        Intent intent = new Intent(this,Info_Un.class);
+        startActivity(intent);
     }
 }
