6f748871591b51d025b1e63bc9bcf5f5db4ff34a
wiranatha98 <wiranatha98@gmail.com>
2019-03-19 23:53:06 +0700

    'memperbaiki kodingan sulthani'

---
+++ app/src/main/java/com/example/edulib/Saran.java
@@ -0,0 +1,16 @@
+package com.example.edulib;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+public class Saran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saran);
+        ActionBar ab = getSupportActionBar();
+        ab.hide();
+    }
+}
--- app/src/main/java/com/example/edulib/Tentang.java
+++ app/src/main/java/com/example/edulib/Tentang.java
@@ -1,16 +1,39 @@
 package com.example.edulib;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Tentang extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tentang);
+        setContentView(R.layout.activity_saran_keluhan);
+        final ImageView saran = findViewById(R.id.imageView13);
+        final ImageView keluhan= findViewById(R.id.imageView17);
         ActionBar ab = getSupportActionBar();
-        ab.setTitle("Tentang");
+        ab.hide();
+
+        saran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Saran.class);
+                startActivity(intent);
+            }
+        });
+
+        keluhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Keluhan.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
