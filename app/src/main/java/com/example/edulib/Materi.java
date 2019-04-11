b5c3ff7efb8a37f619ae3ed924d72c6cbbeb7015
Wiranatha <wiranatha98@gmail.com>
2019-04-11 16:56:45 +0700

    update

--- app/src/main/java/com/example/edulib/Materi.java
+++ app/src/main/java/com/example/edulib/Materi.java
@@ -1,9 +1,11 @@
 package com.example.edulib;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Materi extends AppCompatActivity {
 
@@ -13,14 +15,23 @@ public class Materi extends AppCompatActivity {
         setContentView(R.layout.activity_materi);
         ActionBar ab = getSupportActionBar();
         ab.hide();
+        ImageView ipa = findViewById(R.id.imageView29);
+
     }
 
     public void berandaClick(View view) {
-    }
-
-    public void diskusiClick(View view) {
+        Intent intent= new Intent(getApplicationContext(), Home.class);
+        startActivity(intent);
     }
 
     public void materiClick(View view) {
+        Intent intent= new Intent(getApplicationContext(), Materi.class);
+        startActivity(intent);
     }
+
+    public void diskusiClick(View view) {
+        Intent intent= new Intent(getApplicationContext(), Diskusi.class);
+        startActivity(intent);
+    }
+
 }
