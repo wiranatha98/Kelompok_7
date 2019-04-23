0ee58e2d54ca48c5891fa697b1f168526a9a3329
Satya Wira Yanottama <realsyer@gmail.com>
2019-04-24 02:40:25 +0700

    Code fix

--- app/src/main/java/com/example/edulib/Admin.java
+++ app/src/main/java/com/example/edulib/Admin.java
@@ -38,83 +38,83 @@ public class Admin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 //        saran = new ArrayList<>();
-//
-//        mAuth = FirebaseAuth.getInstance();
-////        keluhan = new ArrayList<>();
-//        arSaran = new ArrayList<>();
-//        arKeluhan = new ArrayList<>();
-//        adapSaran = new AdapterKeluhan(arSaran,this);
-//        adapKeluhan = new AdapterKeluhan(arKeluhan,this);
-////        lSaran = new ListAdapter(saran, Admin.this);
-////        lKeluhan = new ListAdapter(keluhan, Admin.this);
-//        rvSaran = findViewById(R.id.rvSaran);
-//        rvKeluhan = findViewById(R.id.rvKeluhan);
-//        rvSaran.setLayoutManager(new LinearLayoutManager(this));
-//        rvKeluhan.setLayoutManager(new LinearLayoutManager(this));
-//        rvSaran.setAdapter(adapSaran);
-//        rvKeluhan.setAdapter(adapKeluhan);
-//        admin = findViewById(R.id.txAdmin);
-//        admin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(final View v) {
-//                PopupMenu menu = new PopupMenu(v.getContext(), admin);
-//                menu.getMenuInflater()
-//                        .inflate(R.menu.admin, menu.getMenu());
-//                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-//                    @Override
-//                    public boolean onMenuItemClick(MenuItem item) {
-//                        switch (item.getItemId()) {
-//                            case R.id.logout:
-//                                mAuth.signOut();
-//                                startActivity(new Intent(v.getContext(), Login.class));
-//                                finish();
-//                                break;
-//
-//                        }
-//                        return true;
-//                    }
-//                });
-//                menu.show();
-//            }
-//        });
-//
-//
-//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-//        ref.child("Saran").addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                arSaran.clear();
-//                for (DataSnapshot x : dataSnapshot.getChildren()) {
-//                    Log.d("TAG", x.getValue().toString());
-////                    saran.add(new List(x.child("Perihal").getValue().toString(), x.child("keluhan").getValue().toString()));
-//                    arSaran.add(new SaranKeluhan(x.child("perihal").getValue().toString(),x.child("keluhan").getValue().toString(),x.child("nama").getValue().toString()));
-//
-//                }
-//                adapSaran.notifyDataSetChanged();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
-//
-//        ref.child("Keluhan").addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                arKeluhan.clear();
-//                for (DataSnapshot x : dataSnapshot.getChildren()) {
-////                    keluhan.add(new List(x.child("Perihal").getValue().toString(), x.child("keluhan").getValue().toString()));
-////                    lKeluhan.notifyDataSetChanged();
-//                    arKeluhan.add(new SaranKeluhan(x.child("perihal").getValue().toString(),x.child("keluhan").getValue().toString(),x.child("nama").getValue().toString()));
-//                }
-//                adapKeluhan.notifyDataSetChanged();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
+
+        mAuth = FirebaseAuth.getInstance();
+//        keluhan = new ArrayList<>();
+        arSaran = new ArrayList<>();
+        arKeluhan = new ArrayList<>();
+        adapSaran = new AdapterKeluhan(arSaran,this);
+        adapKeluhan = new AdapterKeluhan(arKeluhan,this);
+//        lSaran = new ListAdapter(saran, Admin.this);
+//        lKeluhan = new ListAdapter(keluhan, Admin.this);
+        rvSaran = findViewById(R.id.rvSaran);
+        rvKeluhan = findViewById(R.id.rvKeluhan);
+        rvSaran.setLayoutManager(new LinearLayoutManager(this));
+        rvKeluhan.setLayoutManager(new LinearLayoutManager(this));
+        rvSaran.setAdapter(adapSaran);
+        rvKeluhan.setAdapter(adapKeluhan);
+        admin = findViewById(R.id.txAdmin);
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                PopupMenu menu = new PopupMenu(v.getContext(), admin);
+                menu.getMenuInflater()
+                        .inflate(R.menu.admin, menu.getMenu());
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.logout:
+                                mAuth.signOut();
+                                startActivity(new Intent(v.getContext(), Login.class));
+                                finish();
+                                break;
+
+                        }
+                        return true;
+                    }
+                });
+                menu.show();
+            }
+        });
+
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        ref.child("Saran").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                arSaran.clear();
+                for (DataSnapshot x : dataSnapshot.getChildren()) {
+                    Log.d("TAG", x.getValue().toString());
+//                    saran.add(new List(x.child("Perihal").getValue().toString(), x.child("keluhan").getValue().toString()));
+                    arSaran.add(new SaranKeluhan(x.child("perihal").getValue().toString(),x.child("keluhan").getValue().toString(),x.child("nama").getValue().toString()));
+
+                }
+                adapSaran.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        ref.child("Keluhan").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                arKeluhan.clear();
+                for (DataSnapshot x : dataSnapshot.getChildren()) {
+//                    keluhan.add(new List(x.child("Perihal").getValue().toString(), x.child("keluhan").getValue().toString()));
+//                    lKeluhan.notifyDataSetChanged();
+                    arKeluhan.add(new SaranKeluhan(x.child("perihal").getValue().toString(),x.child("keluhan").getValue().toString(),x.child("nama").getValue().toString()));
+                }
+                adapKeluhan.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
