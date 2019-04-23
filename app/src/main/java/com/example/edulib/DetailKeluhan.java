4ac724113d8120c7a071aff65fd44593425c6034
Satya Wira Yanottama <realsyer@gmail.com>
2019-04-24 02:36:35 +0700

    actvity baru

---
+++ app/src/main/java/com/example/edulib/DetailKeluhan.java
@@ -0,0 +1,13 @@
+package com.example.edulib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailKeluhan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_keluhan);
+    }
+}
